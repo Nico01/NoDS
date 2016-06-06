8f99e0c4e1d95a2fe3e1bdea4f02423ac49e3985
Frederic Meyer <binarynoob96@gmail.com>
2016-06-07 01:54:42 +0200

    Add gba_descriptor

---
+++ src/gba/gba_system.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2016 Frederic Meyer
+ *
+ * This file is part of NoDS.
+ *
+ * NoDS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * NoDS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NoDS. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include "../common/log.h"
+#include "gba_system.h"
+
+system_descriptor gba_descriptor = {
+    .name = "gba",
+    .screen_width = 240,
+    .screen_height = 160,
+    .open = NULL,
+    .close = NULL,
+    .frame = NULL
+};
+
