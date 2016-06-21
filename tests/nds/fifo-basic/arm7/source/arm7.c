8436855c796ce05dfb0aeb09f6037acb59d2d89b
Frederic Meyer <binarynoob96@gmail.com>
2016-06-21 23:35:38 +0200

    Uploaded first NDS test rom

---
+++ tests/nds/fifo-basic/arm7/source/arm7.c
@@ -0,0 +1,28 @@
+#include <nds.h>
+
+#define IPCFIFOCNT *(unsigned short*)0x4000184
+#define IPCFIFOSEND *(unsigned long*)0x4000188
+
+#define FIFO_ENABLE (1 << 15)
+#define FIFO_SEND_FULL 2
+
+int main() {
+    int i = 0;
+
+    irqInit();
+	irqEnable( IRQ_VBLANK);
+
+    // Enable FIFOs
+    IPCFIFOCNT = FIFO_ENABLE;
+
+	// Keep the ARM7 mostly idle
+	while (1) {
+        // Send i over FIFO
+        if (!(IPCFIFOCNT & FIFO_SEND_FULL)) {
+            IPCFIFOSEND = i++;
+        }
+        swiWaitForVBlank();
+	}
+	return 0;
+}
+
---
+++ tests/nds/fifo-basic/arm9/source/arm9.c
@@ -0,0 +1,25 @@
+#include <nds.h>
+#include <stdio.h>
+
+#define IPCFIFOCNT *(unsigned short*)0x4000184
+#define IPCFIFORECV *(unsigned long*)0x4100000
+
+#define FIFO_ENABLE (1 << 15)
+#define FIFO_RECV_EMPTY (1 << 8)
+
+int main(void) {
+	// Enable FIFOs
+    IPCFIFOCNT = FIFO_ENABLE;
+
+    consoleDemoInit();
+    puts("Simple IPC FIFO demo.");
+
+	while(1) {
+        if (!(IPCFIFOCNT & FIFO_RECV_EMPTY)) {
+            printf("%d\n", IPCFIFORECV);
+        }
+		swiWaitForVBlank();
+	}
+
+	return 0;
+}
